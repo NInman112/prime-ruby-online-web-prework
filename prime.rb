4629f301864e6b74bfc8aa19992554729ec64f33
Nick Inman <nick@theoatmeal.com>
2018-11-16 21:37:43 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,11 +2,11 @@
 def prime?(number)
   if number <= 1
     return FALSE
-  elsif number == 2 
-    return TRUE
-  elsif (2..number - 1).each do |x| (number % x) == 0 
+  elsif (2..number - 1).each do |x| 
+    if(number % x) == 0 
       return FALSE
-  else 
-      return TRUE
+    end
+  else
+    return True
   end
 end
