fa5b4e3046a447800519a3b352ae275d11ac33c1
Nick Inman <nick@theoatmeal.com>
2018-11-16 21:17:17 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,5 +3,9 @@ def prime?(number)
   if number == 2 
     TRUE
  elsif number % 3 == 0 || number % 5 == 0 || number % 7 == 0 || number % 11== 0 || number % 13 == 0 || number % 17 == 0
+ TRUE
+ else
+   FALSE
+ end
 
 end
